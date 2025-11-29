2a11c4eb83631e5213355ef35bcb9dc62738ae7e
Carlos271085 <car.bernal@duocuc.cl>
2025-11-29 03:35:45 -0300

    zzzz

---
+++ src/main/java/com/backend3/backend3/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.backend3.backend3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://cuidado-seguro.s3-website-us-east-1.amazonaws.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
